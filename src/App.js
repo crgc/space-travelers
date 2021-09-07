78006c5f880e3fd2c0b8e63f16a813b7f19082b8
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-09-07 17:01:49 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { Switch } from 'react-router';
 import './App.css';
+import Missions from './pages/Missions';
+import Profile from './pages/Profile';
+import NoMatch from './pages/NoMatch';
+import Rockets from './pages/Rockets';
 
 function App() {
   return (
     <div className="App">
-      <h1>SPACE TRAVELER&apos;S HUB</h1>
+      <Switch>
+        <Route exact path="/">
+          <Rockets />
+        </Route>
+        <Route path="/missions">
+          <Missions />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </div>
   );
 }
