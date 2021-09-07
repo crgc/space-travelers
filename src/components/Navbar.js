de79b0d9a745b17e605f740cc745b5aa9428c11c
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-09-07 17:53:30 -0400

    Add style and routing to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,54 @@
-import { Navbar as Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 
-const Navbar = () => {
+const Navigation = () => {
+  const sections = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/profile',
+      text: 'My Profile',
+    },
+  ];
+
   return (
-    <Nav bg="light">
-      <Container>
-        <Nav.Brand href="/">
+    <Navbar className="py-4" bg="light">
+      <Container className="navbar-container d-flex flex-row justify-content-between">
+        <Navbar.Brand href="/">
           <img
             alt="Space Traveler's Hub"
             src="logo.svg"
             width="30"
             height="30"
-            className="d-inline-block align-top mr-2"
+            className="d-inline-block align-top mx-3"
           />{' '}
         SPACE TRAVELER'S HUB
-        </Nav.Brand>
+        </Navbar.Brand>
+        <Nav>
+          {sections.map((section) => (
+            <Nav.Link key={section.id}>
+              <NavLink
+                to={section.path}
+                activeClassName="active-link"
+                exact
+              >
+                {section.text}
+              </NavLink>
+            </Nav.Link>
+            ))}
+        </Nav>
       </Container>
-    </Nav>
+    </Navbar>
   );
 };
 
-export default Navbar;
+export default Navigation;
