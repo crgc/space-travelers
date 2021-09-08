fc858d2c1968a99ca5eafc8f8737c7f6ba08e586
Jocyline Magero <jmagero2017@gmail.com>
2021-09-08 16:38:06 -0500

    Configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import missionsReducer from './missions/missions';
+
+const reducer = combineReducers({
+  missionsReducer,
+});
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger),
+);
+
+export default store;
