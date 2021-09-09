38df5fa92b3b92b20077f40d99f939ff9a69ab0c
Jocyline Magero <jmagero2017@gmail.com>
2021-09-09 14:52:07 -0500

    Add reducer for joining mission

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -8,6 +8,18 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MISSIONS:
       return action.payload;
+    case JOIN_MISSION: {
+      const newState = state.map((mission) => {
+        if (mission.mission_id !== action.id) {
+          return mission;
+        }
+        return {
+          ...mission,
+          reserved: true,
+        };
+      });
+      return newState;
+    }
     case REQUEST_FAILURE:
       return state;
 
