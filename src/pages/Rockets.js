bc823f65c11d41848a5338de4da0e136120e484b
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-09-09 18:13:08 -0400

    Add fetch data logic and style for Rockets page

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,7 +1,32 @@
-const Rockets = () => (
-  <div>
-    <h1>Rockets</h1>
-  </div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Rocket from '../components/rockets/Rocket';
+import { getRockets } from '../redux/rockets/rockets';
+
+const Rockets = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRockets());
+  }, []);
+
+  const rockets = useSelector((state) => state.rocketsReducer.rockets);
+
+  return (
+    <div className="rockets-container mt-4">
+      {
+        rockets.map((rocket) => (
+          <Rocket
+            id={rocket.id}
+            key={rocket.id}
+            name={rocket.name}
+            imageUrl={rocket.image_url}
+            description={rocket.description}
+          />
+        ))
+      }
+    </div>
+  );
+};
 
 export default Rockets;
