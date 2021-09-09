8b2223392c85ade7279bbac8a4f3ebac837d196d
Jocyline Magero <jmagero2017@gmail.com>
2021-09-09 18:07:20 -0500

    Add style to buttons

--- src/components/missions/MissionsDisplay.js
+++ src/components/missions/MissionsDisplay.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { Table, Badge} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'
+import style from './MissionsDisplay.module.css';
 
 const MissionsTable = (props) => {
   const { missions, joinMission, leaveMission } = props;
@@ -19,27 +20,27 @@ const MissionsTable = (props) => {
         <tbody>
         {
                 missions.map((mission) => (
-                  <tr key={mission.mission_id} className="missions">
-                    <td>
+                  <tr key={mission.mission_id} className="mission">
+                    <td style={{fontSize: '12px', fontWeight: '700'}}>
                       {mission.mission_name}
                     </td>
-                    <td style={{ fontSize: '8px' }}>
+                    <td style={{ fontSize: '10px',textAlign: 'justify' }}>
                       {mission.description}
                     </td>
                     <td>
-                         {mission.reserved && (
-                          <Badge>Active Member</Badge>
+                      {mission.reserved && (
+                          <Badge className={style.Badge}>Active Member</Badge>
                       )} 
                       {!mission.reserved && (
-                      <Badge bg="secondary">Not A Member</Badge>
+                      <Badge className={style.Badge2}>Not A Member</Badge>
                       )}
                     </td>
                     <td>
                     {mission.reserved && (
-                      <Button variant="danger" onClick={() => leaveMission(mission.mission_id)}>Leave Mission</Button>
+                      <Button variant="outline-danger" onClick={() => leaveMission(mission.mission_id)}>Leave Mission</Button>
                       )}
                     {!mission.reserved && (
-                        <Button variant="secondary"  onClick={() => joinMission(mission.mission_id)}>Join Mission</Button>
+                        <Button variant="outline-secondary"  onClick={() => joinMission(mission.mission_id)}>Join Mission</Button>
                       )}
                     </td>
                   </tr>
