511cc45c3f0683170c5e8ecd939651693649249a
Jocyline Magero <jmagero2017@gmail.com>
2021-09-10 12:07:19 -0500

    Get rockets and missions from the store

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux';
 import MyItems from '../components/profile/MyItems';
 
-const Profile = () => (
-  <div className="d-flex flex-row justify-content-around mt-3 profile-container">
+  const storeMissions = useSelector((state) => state.missionsReducer);
+  const rockets = useSelector((state) => state.rocketsReducer);
     <MyItems title="My Missions" items={[{ id: 1, name: 'Telstar' }]} />
     <MyItems title="My Rockets" items={[{ id: 1, name: 'Falcon 1' }]} />
   </div>
