960005fb8746f8147ba98ba6d54a4ad999d46207
Carlos Gonzalez <carlosfwrd@gmail.com>
2021-09-10 10:50:30 -0400

    Add reserved badge

--- src/components/rockets/Rocket.js
+++ src/components/rockets/Rocket.js
@@ -1,4 +1,4 @@
-import { Button } from 'react-bootstrap';
+import { Button, Badge } from 'react-bootstrap';
 
 const Rocket = (props) => {
   const { name, description, imageUrl } = props; /* eslint-disable-line react/prop-types */
@@ -8,7 +8,10 @@ const Rocket = (props) => {
       <img className="rocket-img mx-3" src={imageUrl} alt="" />
       <div className="rocket-info">
         <h3 className="rocket-name">{ name }</h3>
-        <p>{ description }</p>
+        <p>
+          <Badge className="rocket-reserved">Reserved</Badge>
+          { description }
+        </p>
         <Button>Reserve Rocket</Button>
       </div>
     </article>
