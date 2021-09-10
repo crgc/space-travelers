ff400f4d48c5e39d84c61a6f861966a7dbdcf108
Jocyline Magero <jmagero2017@gmail.com>
2021-09-10 12:08:14 -0500

    Filter reserved rockects

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import MyItems from '../components/profile/MyItems';
 
+const Profile = () => {
   const storeMissions = useSelector((state) => state.missionsReducer);
   const rockets = useSelector((state) => state.rocketsReducer);
 
@@ -9,5 +10,14 @@ import MyItems from '../components/profile/MyItems';
       id: mission.mission_id,
       name: mission.mission_name,
     }));
+
+  const rocketsReserved = rockets.filter((rocket) => rocket.reserved);
+  return (
+    <div className="d-flex flex-row justify-content-around mt-3 profile-container">
+      <MyItems title="My Missions" items={missionsReverved} />
+      <MyItems title="My Rockets" items={rocketsReserved} />
+    </div>
+  );
+};
 
 export default Profile;
